b4015ac398ff586d0c7fa6fc632831548c38f87b
Zachary Lehmann <zlehmann@gmail.com>
2019-01-03 21:22:48 +0000

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+
+  def hello_world
+    render "some_page"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/hello_world', to: 'static#hello_world'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
